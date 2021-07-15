efa772ac165c8f1b87a425903ccd2b1631bfdbad
Arjun <gt01arjun@thecea.ca>
2021-07-15 02:32:08 -0700

    Restart For Testing

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,6 +11,13 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private GameObject _gameCamera;
 
+    private Vector3 _ballInitialPos;
+
+    private void Start()
+    {
+        _ballInitialPos = _ball.transform.position;
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Return))
@@ -23,6 +30,13 @@ public class GameManager : MonoBehaviour
             _ball.SetActive(true);
             _prepCamera.SetActive(false);
             _gameCamera.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            _ball.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            _ball.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            _ball.transform.position = _ballInitialPos;
         }
     }
 }
